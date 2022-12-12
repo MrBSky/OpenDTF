e5a47e075881a1843ec0e683bf178b2a717b8150
Sirapop Ruankham <sirapop.ru@gmail.com>
2022-12-12 20:10:38 +0700

    opendtf

---
+++ src/main.cpp
@@ -0,0 +1,244 @@
+#include <Arduino.h>
+#include <Encoder.h>
+#include <analogWrite.h>
+#include <JC_Button_ESP.h>
+
+unsigned long period = 1000;
+unsigned long last_time = 0;
+
+#define ASF_PIN 2
+#define PE_PIN 4
+#define FEED_IN_PIN 23
+#define FEED_OUT_PIN 22
+#define encoderA 18
+#define encoderB 19
+
+#define LED 32
+#define BTN_READY 33
+#define BTN_IN 34
+#define BTN_OUT 35
+
+#define LED_PRINT 25
+#define BTN_PRINT 26
+#define BTN_CLEAN 27
+
+#define ASF_DUTY 128
+#define ASF_TIMER_BIT 8
+#define ASF_BASE_FREQ 10
+#define ASF_PHASE 0
+
+#define Ready 1
+#define Standby 0
+
+long oldPosition = -999;
+long newPosition;
+long encoder_active = 0;
+int offsetnewPosition = 50;
+
+long arr_stop[20];
+int chk_stop = 0;
+int offset_chk_stop = 3000;
+uint8_t indexof_arr_stop = 0;
+
+uint8_t PE_state = 0;
+uint8_t btn_pe_state = 0;
+
+uint8_t last_value0 = 0, last_value1 = 0, last_value2 = 0;
+uint8_t cur_ready = 0, cur_in = 0, cur_out = 0;
+
+Encoder epsonEncoder(encoderA, encoderB);
+
+enum state
+{
+  p_standby,
+  p_readytoprint,
+  p_print,
+  p_printfinish,
+  p_stepfinish
+};
+
+enum state process_state = p_standby;
+
+xTaskHandle TaskHandle_1;
+xTaskHandle TaskHandle_2;
+xTaskHandle TaskHandle_3;
+
+void readme()
+{
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("**********************************");
+  Serial.println("* Hello,                         *");
+  Serial.println("* DFT Roll To Roll Controller    *");
+  Serial.println("* It was developed by mr.blueSky *");
+  Serial.println("* Facebook: Sirapop Ruankham     *");
+  Serial.println("* Website: www.oldmanlaser.com   *");
+  Serial.println("**********************************");
+  Serial.println("");
+}
+
+void Ready_To_Print()
+{
+  switch (process_state)
+  {
+
+  case p_stepfinish:
+
+    break;
+
+  case p_standby:
+
+    break;
+
+  case p_readytoprint:
+
+    if (newPosition >= encoder_active + offsetnewPosition)
+    {
+      digitalWrite(PE_PIN, false);
+      digitalWrite(LED, true);
+
+      vTaskDelay(5000 / portTICK_PERIOD_MS);
+      process_state = p_print;
+    }
+
+    break;
+
+  case p_print:
+
+    if (chk_stop > offset_chk_stop)
+    {
+      process_state = p_printfinish;
+      btn_pe_state = Standby;
+
+      digitalWrite(PE_PIN, true);
+      digitalWrite(LED, false);
+    }
+
+    break;
+
+  case p_printfinish:
+
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    process_state = p_standby;
+    break;
+  }
+
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+}
+
+void BTN_task(void *p)
+{
+  while (1)
+  {
+    cur_ready = digitalRead(BTN_READY);
+    cur_in = digitalRead(BTN_IN);
+    cur_out = digitalRead(BTN_OUT);
+
+    if (cur_ready != last_value0)
+    {
+      if (cur_ready == 0 && btn_pe_state == Standby)
+      {
+        btn_pe_state = Ready;
+        encoder_active = newPosition;
+        process_state = p_readytoprint;
+
+        digitalWrite(LED, true);
+
+        if (!digitalRead(LED_PRINT))
+        {
+          digitalWrite(BTN_PRINT, true);
+          vTaskDelay(100 / portTICK_PERIOD_MS);
+          digitalWrite(BTN_PRINT, false);
+        }
+      }
+      else if (cur_ready == 0 && btn_pe_state == Ready)
+      {
+        btn_pe_state = Standby;
+        process_state = p_standby;
+
+        digitalWrite(PE_PIN, true);
+        digitalWrite(LED, false);
+      }
+      last_value0 = cur_ready;
+    }
+
+    if (cur_in != last_value1)
+    {
+      if (cur_in == 0 && btn_pe_state == Standby)
+      {
+        digitalWrite(FEED_IN_PIN, false);
+      }
+      else
+      {
+        digitalWrite(FEED_IN_PIN, true);
+      }
+      last_value1 = cur_in;
+    }
+
+    if (cur_out != last_value2)
+    {
+      if (cur_out == 0 && btn_pe_state == Standby)
+      {
+        digitalWrite(FEED_IN_PIN, false);
+        digitalWrite(FEED_OUT_PIN, false);
+      }
+      else
+      {
+        digitalWrite(FEED_IN_PIN, true);
+        digitalWrite(FEED_OUT_PIN, true);
+      }
+      last_value2 = cur_out;
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void ASF_Emulator()
+{
+  analogWrite(ASF_PIN, ASF_DUTY, ASF_BASE_FREQ, ASF_TIMER_BIT, ASF_PHASE);
+  // printPinsStatus();
+}
+
+void Encoder_Task(void *p)
+{
+  while (1)
+  {
+    newPosition = epsonEncoder.read();
+    if (newPosition != oldPosition)
+    {
+      oldPosition = newPosition;
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup()
+{
+  readme();
+
+  ASF_Emulator();
+
+  pinMode(BTN_IN, INPUT);
+  pinMode(BTN_OUT, INPUT);
+  pinMode(BTN_READY, INPUT);
+  pinMode(LED, OUTPUT);
+
+  pinMode(BTN_PRINT, OUTPUT);
+  pinMode(BTN_CLEAN, OUTPUT);
+  pinMode(LED_PRINT, INPUT);
+
+  pinMode(ASF_PIN, OUTPUT);
+  pinMode(PE_PIN, OUTPUT);
+  pinMode(FEED_IN_PIN, OUTPUT);
+  pinMode(FEED_OUT_PIN, OUTPUT);
+  digitalWrite(PE_PIN, true);
+
+  xTaskCreate(&Encoder_Task, "Encoder_Task", 2048, NULL, 10, NULL);
+  xTaskCreate(&BTN_task, "BTN_task", 2048, NULL, 10, NULL);
+}
+
+void loop()
+{
+  Ready_To_Print();
+}
